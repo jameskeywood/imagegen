bb3ba2f9e95b38ab679b173ba6cb148ed5570b51
jameskeywood <jskeywood@gmail.com>
2021-10-13 19:05:49 +0100

    new research folder for seperate dataset

--- research/DCGAN - FLOWERS/dcgan.py
+++ research/DCGAN - FLOWERS/dcgan.py
@@ -1,29 +1,82 @@
+# ensure that CUDA and cuDNN files can be found
 import os
-
 os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.0/bin")
 os.add_dll_directory("C:/Users/James/Documents/cuda/bin")
 
+# imports for program
 import tensorflow as tf
-
-tf.__version__
-
-import glob
-import imageio
-import matplotlib.pyplot as plt
-import numpy as np
-import os
-import PIL
+import tensorflow_datasets as tfds
+import tensorflow_docs.vis.embed as embed
 from tensorflow.keras import layers
+import numpy as np
 import time
+import PIL
+import PIL.Image
+import imageio
+import glob
+import pathlib
+import matplotlib.pyplot as plt
 from IPython import display
 
-(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+# variables
+results_dir = "output/"
 
-train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+# allow numpy arrays to be reshaped
+from tensorflow.python.ops.numpy_ops import np_config
+np_config.enable_numpy_behavior()
+        
+# print TensorFlow version
+print("Version:", tf.__version__)
+
+# download, if not already, the flower dataset
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+
+data_dir = tf.keras.utils.get_file(origin=dataset_url,
+                                   fname='flower_photos',
+                                   untar=True)
+
+# locate and print dataset directory
+path = pathlib.Path(data_dir)
+print("Dataset directory:", path)
+
+# calculate and print amount of images in dataset
+image_count = len(list(path.glob('*/*.jpg')))
+print("Image count:", image_count)
+
+###
+
+# load flower dataset
+train_images = tf.keras.preprocessing.image_dataset_from_directory(path)
+
+# find class names and print them
+class_names = train_images.class_names
+print(class_names)
+
+# select dataset class
+class_num = 3
+train_images = train_images.batch(class_num)
+
+data_dir = os.path.join(data_dir, class_names[class_num])
+train_images = []
+for _, file in enumerate(os.listdir(data_dir)):
+    temp_dir = os.path.join(data_dir, file)
+    image = PIL.Image.open(temp_dir)
+    # format the image suitably for processing
+    newsize = (28, 28)
+    image = image.resize(newsize) # resize
+    image = image.convert('L') # greyscale
+    data = np.asarray(image, dtype=np.float32)
+    train_images.append(data)
+    break
+
+# Convert data into Tensor
+train_images = tf.convert_to_tensor(train_images)
+
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1)
 train_images = (train_images - 127.5) / 127.5  # Normalize the images to [-1, 1]
 
 BUFFER_SIZE = 60000
-BATCH_SIZE = 256
+BATCH_SIZE = 1000
 
 # Batch and shuffle the data
 train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
@@ -77,7 +130,7 @@ def make_discriminator_model():
 
 discriminator = make_discriminator_model()
 decision = discriminator(generated_image)
-print (decision)
+print(decision)
 
 # This method returns a helper function to compute cross entropy loss
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
@@ -101,9 +154,9 @@ checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
                                  generator=generator,
                                  discriminator=discriminator)
 
-EPOCHS = 100
+EPOCHS = 10
 noise_dim = 100
-num_examples_to_generate = 16
+num_examples_to_generate = 1
 
 # You will reuse this seed overtime (so it's easier)
 # to visualize progress in the animated GIF)
@@ -160,15 +213,16 @@ def generate_and_save_images(model, epoch, test_input):
   # This is so all layers run in inference mode (batchnorm).
   predictions = model(test_input, training=False)
 
-  fig = plt.figure(figsize=(4, 4))
+  fig = plt.figure(figsize=(1, 1))
 
   for i in range(predictions.shape[0]):
-      plt.subplot(4, 4, i+1)
+      plt.subplot(1, 1, i+1)
       plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
       plt.axis('off')
 
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-  # plt.show() # uncomment to display image every epoch
+  if not os.path.isdir(results_dir):
+    os.makedirs(results_dir)
+  plt.savefig('{}image_at_epoch_{:04d}.png'.format(results_dir, epoch))
 
 train(train_dataset, EPOCHS)
 
@@ -176,14 +230,15 @@ checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
 # Display a single image using the epoch number
 def display_image(epoch_no):
-  return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+  return PIL.Image.open('{}image_at_epoch_{:04d}.png'.format(results_dir, epoch_no))
 
 display_image(EPOCHS)
 
-anim_file = 'dcgan.gif'
+# generate GIF at the end of program
+anim_file = results_dir + 'dcgan.gif'
 
 with imageio.get_writer(anim_file, mode='I') as writer:
-  filenames = glob.glob('image*.png')
+  filenames = glob.glob(results_dir + 'image*.png')
   filenames = sorted(filenames)
   for filename in filenames:
     image = imageio.imread(filename)
@@ -191,27 +246,4 @@ with imageio.get_writer(anim_file, mode='I') as writer:
   image = imageio.imread(filename)
   writer.append_data(image)
 
-import tensorflow_docs.vis.embed as embed
 embed.embed_file(anim_file)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
