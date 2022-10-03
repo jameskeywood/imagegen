030fd49945d2e33192e5cf7e3e451ba74a34111f
jameskeywood <jskeywood@gmail.com>
2022-10-03 21:34:59 +0100

    added new iteration in stable diffusion

--- stable_diffusion/README.md
+++ stable_diffusion/README.md
@@ -6,6 +6,11 @@
 ![Stable Diffusion](https://github.com/jameskeywood/geno/blob/main/stable_diffusion/StableDiffusion.png?raw=true)
 
 ## Resources
+Using stable diffusion in Python: https://analyticsindiamag.com/how-to-generate-an-image-from-text-using-stable-diffusion-on-python/
+Working with stable diffusion repository: https://www.assemblyai.com/blog/how-to-run-stable-diffusion-locally-to-generate-images/
+Assistance with PyTorch and CUDA: https://stackoverflow.com/questions/57814535/assertionerror-torch-not-compiled-with-cuda-enabled-in-spite-upgrading-to-cud
 
 ## Versions
+CUDA Toolkit v11.7.1
+PyTorch v1.12.1
 
---
+++ stable_diffusion/iterations/221003/sd.py
@@ -0,0 +1,31 @@
+# check GPU support
+# !nvidia-smi
+# install the dependencies in the command line
+# !pip install diffusers==0.3.0
+# !pip install transformers scipy ftfy
+# !pip install ipywidgets==7.7.2
+
+# importing the dependencies
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+from PIL import Image
+
+# hugging face access token
+access_token = "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"
+
+# creating the pipeline
+experimental_pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16, use_auth_token=access_token) 
+
+# shift the pipleline to the GPU accelerator
+experimental_pipe = experimental_pipe.to("cuda")
+
+# user input
+user_input = input("Text prompt: ")
+
+# generating the image
+description = user_input
+with autocast("cuda"):
+    image = experimental_pipe(description).images[0]  
+# now to display an image you can do either save it such as:
+image.save(f"output.png")
